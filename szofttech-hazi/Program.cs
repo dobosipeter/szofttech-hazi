3d186f68536a5429b4caf2454df1106b2ac68cae
Dobosi Péter <dobosipeter@edu.bme.hu>
2020-12-04 13:59:17 +0100

    Add project files.

---
+++ szofttech-hazi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace szofttech_hazi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
