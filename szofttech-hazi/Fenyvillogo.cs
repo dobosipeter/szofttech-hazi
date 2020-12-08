9ea509ec894e63e232bbfee8540a64de55979a54
Dobosi Péter <dobosipeter@edu.bme.hu>
2020-12-08 15:32:38 +0100

    Fenyvillogo added.

--- szofttech-hazi/Fenyvillogo.cs
+++ szofttech-hazi/Fenyvillogo.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace szofttech_hazi
 {
-    class Fenyvillogo
+    class Fenyvillogo : IRiasztasiKezelo
     {
+        VezerloPanel vezerloPanel;
+        private bool egyszeruRiasztas;
+        private bool tuzJelzesRiasztas;
+        bool[] allapotok;
+
+        public Fenyvillogo(VezerloPanel vezerloPanel)
+        {
+            this.vezerloPanel = vezerloPanel;
+            vezerloPanel.RiasztasiKezeloHozaadasa(this);
+            allapotok = new bool[2];
+        }
+
+        public void Frissites()
+        {
+            allapotok = vezerloPanel.GetRiasztasiAllapot();
+        }
+
+        public void Riasztas()
+        {
+            if (allapotok[0])
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Riasztás!");
+            }
+        }
     }
 }
---
+++ szofttech-hazi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace szofttech_hazi
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Riasztorendszer riasztorendszer = new Riasztorendszer();
+        }
+    }
+}
